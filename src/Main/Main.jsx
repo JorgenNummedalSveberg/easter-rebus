c31f4ea79919695d681999da4897d4f20dab4409
Jørgen Nummedal Sveberg <jorgen.n.sveberg@gmail.com>
2023-01-31 10:04:29 +0100

    added some paths, needs css tweaks

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -12,11 +12,21 @@ function Main() {
             {(item, index) =>
                 <div class={styles.Row}>
                     <Show when={index()%2!==0}>
-                        <div></div>
+                        <svg style="background-color:green; height: 500px; width: 200px"  viewBox="0 0 200 200" preserveAspectRatio="none">
+                            <g fill="none" stroke="black" stroke-width="4">
+                                <path stroke-dasharray="40,5" d="M 200 100 c -50 0, -100 50, -100 100" />
+                            </g>
+                            Sorry, your browser does not support inline SVG.
+                        </svg>
                     </Show>
                     <Mystery mystery={item}/>
                     <Show when={index()%2===0}>
-                        <div></div>
+                        <svg style="background-color:green; height: 500px; width: 200px"  viewBox="0 0 200 200" preserveAspectRatio="none">
+                            <g fill="none" stroke="black" stroke-width="4">
+                                <path stroke-dasharray="40,5" d="M 0 100 c 50 0, 100 50, 100 100" />
+                            </g>
+                            Sorry, your browser does not support inline SVG.
+                        </svg>
                     </Show>
                 </div>
             }
